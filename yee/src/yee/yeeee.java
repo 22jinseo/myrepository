a9ea3f17d5704ecbbe59e18d3564a7a73bf2ef04
admin <admin@DESKTOP-I6HUGBR>
2024-12-05 14:26:50 +0900

    Add a println line.

--- yee/src/yee/yeeee.java
+++ yee/src/yee/yeeee.java
@@ -3,5 +3,6 @@ package yee;
 public class yeeee {
     public static void main(String[] args) {
     	System.out.println("Hello Git!");
+    	System.out.println("This is the 2nd version");
     }
 }
